2a65e67ce77d89a7f53a6e047faf385b7357c525
andela-akangethe <alex.kangethe@andela.com>
2016-04-07 15:47:51 +0300

    Learn about the self object

---
+++ self.rb
@@ -0,0 +1,11 @@
+# Either works
+
+puts "Who's my reciever?"
+
+puts("Who's my reciever?")
+
+puts self
+
+puts self.class
+
+# TAke away every methood is called on an object even if you can't see it
