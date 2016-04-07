bea5fc52d1f0908be55bc989d049b68964379849
Lexville <lexville@Lexvilles-MacBook-Pro.local>
2016-04-07 15:00:38 +0300

    Learning Ruby

---
+++ flicks.rb
@@ -0,0 +1,33 @@
+puts 'Alex loves Boston Legal'
+
+puts 'Mike\'s favorite movie is Goonies'
+
+movie = 'Goonies'
+
+puts 'Mike\'s favorite movie is ' + movie
+
+puts "Mike's favorite movie is #{movie}"
+
+rank = 20
+
+puts movie + ' has been ranked the ' + rank.to_s + 'th best movie of all time'
+
+puts "#{movie} has been ranked the #{rank}th best movie of all time"
+
+puts "I am #{rank / 2 } years old"
+
+my_favourite_show = "Boston Legal"
+
+your_favourite_show = my_favourite_show
+
+my_favourite_show[0] = 'M'
+
+puts your_favourite_show
+
+puts my_favourite_show
+
+my_favourite_show = "Charmed"
+
+puts my_favourite_show
+
+puts your_favourite_show
---
+++ hello_ruby.rb
@@ -0,0 +1,10 @@
+comment = "Ruby is fun!"
+
+puts comment.upcase
+
+# To print out a message three times
+3.times do
+    puts comment.downcase
+end
+
+puts Time.new
